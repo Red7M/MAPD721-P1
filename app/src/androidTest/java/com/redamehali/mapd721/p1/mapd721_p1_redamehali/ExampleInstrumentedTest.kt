a4c746ee4b4c0a977ef7b952d5eb79915bec7e48
Red7M <reda.mehali@gmail.com>
2021-04-15 17:53:10 -0400

    Initializes project

---
+++ app/src/androidTest/java/com/redamehali/mapd721/p1/mapd721_p1_redamehali/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package com.redamehali.mapd721.p1.mapd721_p1_redamehali
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.redamehali.mapd721.p1.mapd721_p1_redamehali", appContext.packageName)
+    }
+}
---
+++ app/src/main/java/com/redamehali/mapd721/p1/mapd721_p1_redamehali/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.redamehali.mapd721.p1.mapd721_p1_redamehali
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
