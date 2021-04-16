20ea6b4bb3e4c899290468811f875277e348cff4
Red7M <reda.mehali@gmail.com>
2021-04-15 22:45:37 -0400

    Adds sign in button through FirebaseUI authentication

---
+++ app/src/main/java/com/redamehali/mapd721/p1/mapd721_p1_redamehali/HomeScreenActivity.kt
@@ -0,0 +1,15 @@
+package com.redamehali.mapd721.p1.mapd721_p1_redamehali
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * Created by Reda Mehali on 4/15/21.
+ */
+class HomeScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.home_screen_activity)
+    }
+}
--- app/src/main/java/com/redamehali/mapd721/p1/mapd721_p1_redamehali/MainActivity.kt
+++ app/src/main/java/com/redamehali/mapd721/p1/mapd721_p1_redamehali/MainActivity.kt
@@ -1,11 +1,107 @@
 package com.redamehali.mapd721.p1.mapd721_p1_redamehali
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.android.gms.common.SignInButton
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+
+        private const val RC_SIGN_IN = 123
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val signInButton = findViewById<SignInButton>(R.id.signInButton)
+        signInButton.setOnClickListener {
+            createSignInIntent()
+        }
+    }
+
+    private fun createSignInIntent() {
+        // Choose authentication providers
+        val providers = arrayListOf(AuthUI.IdpConfig.GoogleBuilder().build())
+
+        // Create and launch sign-in intent
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .build(),
+                RC_SIGN_IN)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val response = IdpResponse.fromResultIntent(data)
+
+            if (resultCode == Activity.RESULT_OK) {
+                // Successfully signed in
+                val user = FirebaseAuth.getInstance().currentUser
+                // Launch Home screen
+            } else {
+                // Sign in failed.
+            }
+        }
+    }
+
+    private fun signOut() {
+        // [START auth_fui_signout]
+        AuthUI.getInstance()
+                .signOut(this)
+                .addOnCompleteListener {
+                    // ...
+                }
+        // [END auth_fui_signout]
+    }
+
+    private fun delete() {
+        // [START auth_fui_delete]
+        AuthUI.getInstance()
+                .delete(this)
+                .addOnCompleteListener {
+                    // ...
+                }
+        // [END auth_fui_delete]
+    }
+
+    private fun themeAndLogo() {
+        val providers = emptyList<AuthUI.IdpConfig>()
+
+        // [START auth_fui_theme_logo]
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .setLogo(R.drawable.android_logo) // Set logo drawable
+                        .setTheme(R.style.MyAppTheme) // Set theme
+                        .build(),
+                RC_SIGN_IN)
+        // [END auth_fui_theme_logo]
+    }
+
+    private fun privacyAndTerms() {
+        val providers = emptyList<AuthUI.IdpConfig>()
+        // [START auth_fui_pp_tos]
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .setTosAndPrivacyPolicyUrls(
+                                "https://example.com/terms.html",
+                                "https://example.com/privacy.html")
+                        .build(),
+                RC_SIGN_IN)
+        // [END auth_fui_pp_tos]
     }
 }
